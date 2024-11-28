aea2a9ca625dc5fb26e30414d799f69b8d806a99
latifvaliyev <102619604+latifvaliyev@users.noreply.github.com>
2024-11-28 21:24:03 +0000

    7 fixed sign up and edit for new params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :uni_id])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password, :uni_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :uni_id, :graduation_year, :industry, :linkedin, :bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password, :uni_id, :graduation_year, :industry, :linkedin, :bio])
   end
 end
